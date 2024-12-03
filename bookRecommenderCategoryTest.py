898012196a758b03c24d4b63a5d3fab82325578d
willk <williamdupuis0108@gmail.com>
2024-12-02 19:46:10 -0500

    update

--- bookRecommenderCategoryTest.py
+++ bookRecommenderCategoryTest.py
@@ -7,12 +7,10 @@ AUTH = ("neo4j", "password")
 #Fixed to not return duplicates but was just 2 print statements lol
 
 query = """
-        MATCH (b:Book), (b2:Book)
-        WHERE $category IN b.categories
-        AND $category IN b2.categories
-        AND b.title <> b2.title
-        RETURN b.title AS book, b2.title AS book2
-        LIMIT 20;
+        MATCH (b:Book)
+        WHERE b.categories = $category
+        RETURN b.title AS book
+        LIMIT 10;
         """
 
 # Connect to the database
@@ -22,11 +20,8 @@ def gather_similar_books(category_name):
     category_name = f"['{category_name}']"
     print(category_name)
     with driver.session() as session:
-
         result = session.run(query, category=category_name)
-        
-
-        books = [(record["book"], record["book2"]) for record in result]
+        books = [(record["book"]) for record in result]
     return books
 
 
@@ -39,8 +34,8 @@ book_ratings = gather_similar_books(title)
 # Display the the books and they're respective reviews.
 if book_ratings:
     for book in book_ratings:
-        print(f"Book: {book[0]}, Shares category With: {book[1]}")
+        print(f"Book: {book}")
 else:
     print("No results found for this title.") 
 
-driver.close()
+driver.close()	#1910279
