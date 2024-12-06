b8e3fa0b84665c5e49894afecbc71edc0945b158
willk <williamdupuis0108@gmail.com>
2024-12-05 19:23:43 -0500

    now with cherryPy

--- merging.py
+++ merging.py
@@ -1,21 +1,20 @@
-#bookrecommendercategorytest.py is the outer, while book recommender is the inner 
-
 from neo4j import GraphDatabase
-import csv
-
+import os
+import cherrypy
+#https://docs.cherrypy.dev/en/latest/index.html
 # Database connection
 URI = "bolt://localhost:7687"
 AUTH = ("neo4j", "password")
 
-#Fixed to not return duplicates but was just 2 print statements lol
+# Queries
 query = """
         MATCH (b:Book)
-        WHERE b.categories = $category
+        WHERE b.title CONTAINS $title 
         RETURN b.title AS book
+        ORDER BY rand()
         LIMIT 20;
         """
 
-#this will be for ratings 
 query2 = """
         MATCH (b:Book),(r:Rating)
         WHERE b.title = $title AND r.title = $title
@@ -23,36 +22,20 @@ query2 = """
         LIMIT 20;
         """
 
-# Connect to the database
-driver = GraphDatabase.driver(URI, auth=AUTH)
-
-#function to gather similar books based on category, will figure out how to make this a more dynamic function so the user can choose between author or category etc. 
-def gather_similar_books(category_name):
-
-    category_name = f"['{category_name}']"
-
-
+# Database functions
+def gather_similar_books(title_name):
+    print(title_name)
     with driver.session() as session:
-        result = session.run(query, category=category_name)
-        books = [(record["book"]) for record in result]
-
+        result = session.run(query, title=title_name)
+        books = [record["book"] for record in result]
+    
     return books
 
-#try user input for the title name
-user_input = input("Enter the category: ")
-gathered_books = gather_similar_books(user_input) #calls the function and passes the users input  
-
-
-#before displaying books we want to use the ratings calculations to determine which books are worth displaying. 
 def return_score(title_name):
-
-    title_name = f"{title_name}" 
-
     with driver.session() as session:
-
         result = session.run(query2, title=title_name)
 
-        # Extract the books from the result. Logged originally after it kept failing and realized it returned an object
+        # Extract ratings scores from the query
         scores = [(record["Rating"]) for record in result]
         score = 0.0
         for book in scores:
@@ -61,24 +44,59 @@ def return_score(title_name):
         score = score/len(scores)
         return score
 
-# Display the the books
-if gathered_books:
-    i = 0
-    for book in gathered_books:
-        
-        if i > 4:
-            exit()
+# Connect to the database
+driver = GraphDatabase.driver(URI, auth=AUTH)
 
+class DisplayResults:
+    @cherrypy.expose
+    # cherrypy to create a simple input form to gather the title.
+    def index(self):
+        return """
+                    <head> 
+                        <link href="/style.css" rel="stylesheet">
+                    </head>
+                    <form method="post" action="results">
+                      Enter a title: <input type="text" name="title" />
+                      <button type="submit">Submit</button>
+                    </form>"""
+    
+
+    #second page to display the results page
+    @cherrypy.expose
+    def results(self, title):
+        gathered_books = gather_similar_books(title)
+
+        if gathered_books:
+            # starts creating a long html code block with output
+            output = "<h1>Recommended Books</h1><ul>"
+            recommended_count = 0
+
+            for book in gathered_books:
+                # only want 5 recomendations
+                if recommended_count >= 5:
+                    break
+                #unfortunately long execution time
+                score = return_score(book)
+                if score >= 4.0:
+                    recommended_count += 1
+                    output += f"<li>{book} - Rating: {score:.2f}</li>"
+
+            output += "</ul>" # end of the html list
+            return output
         else:
-            score = return_score(book) #calls the function and passes the book title
-            if score >= 4.0:
-                i += 1
-                print(f"Book: {book} with rating: {score:.2f}") #rounding the score to 2 decimal places
-            else:
-                continue
+            return "<h1>No results found for this title.</h1>"
 
-else:
-    print("No results found for this title.")  
+if __name__ == "__main__":
+    conf = {
+        '/': {
+            'tools.sessions.on': True,
+            'tools.staticdir.root': os.path.abspath(os.getcwd())
+        },
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': './public'
+        }
+    }
+    cherrypy.quickstart(DisplayResults())
 
-#closes the connection to the database
 driver.close()
